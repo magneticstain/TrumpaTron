ade690cd10ed81616d16adab8a5275716fca68a4
Josh Carlson <magneticstain@gmail.com>
2017-03-02 18:12:06 -0500

    Travis-CI Update #5

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Setup.py
+
+APP: Trumpatron
+DESC: 
+CREATION_DATE: 2017-03-02
+
+"""
+
+from distutils.core import setup
+
+setup(name='TrumpaTron',
+      version='1.0.0',
+      description='Donald Trump bot',
+      author='Joshua Carlson-Purcell',
+      author_email='josh@carlson.ninja',
+      url='https://github.com/magneticstain/TrumpaTron'
+     )
