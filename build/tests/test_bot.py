d7a8bf2e01ea9459362a9db63ff2097741f1d4d5
Josh Carlson <magneticstain@gmail.com>
2017-03-02 20:56:27 -0500

    Update Unit Tests

--- build/tests/test_bot.py
+++ build/tests/test_bot.py
@@ -22,3 +22,18 @@ def testConnectToTwitterAPI_ALL_BLANKS():
     api = lib.bot.connectToTwitterAPI('', '', '', '')
 
     assert isinstance(api, tweepy.API)
+
+def testFormatTweet_BLANK_TWEET():
+    with pytest.raises(ValueError):
+        lib.bot.formatTweet('')
+
+def testFormatTweet_PLAIN_TWEET():
+    tweet = 'Test of normal tweet!'
+
+    assert tweet == lib.bot.formatTweet(tweet)
+
+def testFormatTweet_FORMATTED_TWEET():
+    origTweet = 'Test of tweet that needs formatting! https://t.co/RDO6Jt2pip     '
+    postFormattingTweet = 'Test of tweet that needs formatting!'
+
+    assert postFormattingTweet == lib.bot.formatTweet(origTweet)
