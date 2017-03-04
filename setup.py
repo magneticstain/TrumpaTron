17a2a198c0600baa01462f19d2812e2788bde371
Josh Carlson <magneticstain@gmail.com>
2017-03-03 20:12:20 -0500

    Setuptools Update #1

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ CREATION_DATE: 2017-03-02
 
 """
 
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
     name='TrumpaTron',
     version='1.0.0',
     description='Donald Trump bot',
     author='Joshua Carlson-Purcell',
     author_email='josh@carlson.ninja',
-    url='https://github.com/magneticstain/TrumpaTron'
-    packages=setuptools.find_packages(exclude=['build', 'conf']),
+    url='https://github.com/magneticstain/TrumpaTron',
+    packages=find_packages(exclude=['build', 'conf']),
 )
